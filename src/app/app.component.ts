fd1735feb1403551bd7a05c095bc839935dcc13e
Bartha Levente <b.levi.levi@gmail.com>
2025-09-06 11:49:32 +0200

    Működő főoldal. Egyéb még nincs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MainpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
